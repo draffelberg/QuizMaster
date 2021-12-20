8b43c56081862945608904f9a876cbb14bb9fb52
nnakhsaz <96247472+nnakhsaz@users.noreply.github.com>
2021-12-20 16:23:24 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,50 @@
+window.onload = function() {
+
+    var a = document.getElementById('f1a');
+    var b = document.getElementById('f1b');
+    var c = document.getElementById('f1c');
+    var d = document.getElementById('f1d');
+    var e = document.getElementById('f2a');
+    var f = document.getElementById('f2b');
+    var g = document.getElementById('f2c');
+    var h = document.getElementById('f2d');
+    var i = document.getElementById('f3a');
+    var j = document.getElementById('f3b');
+    var k = document.getElementById('f3c');
+    var l = document.getElementById('f3d');
+    var m = document.getElementById('f4a');
+    var n = document.getElementById('f4b');
+    var o = document.getElementById('f4c');
+    var p = document.getElementById('f4d');
+    var q = document.getElementById('f5a');
+    var r = document.getElementById('f5b');
+    var s = document.getElementById('f5c');
+    var t = document.getElementById('f5d');
+
+    a.onclick = handler;
+    b.onclick = handler;
+    c.onclick = handler;
+    d.onclick = handler;
+    e.onclick = handler;
+    f.onclick = handler;
+    g.onclick = handler;
+    h.onclick = handler;
+    i.onclick = handler;
+    j.onclick = handler;
+    k.onclick = handler;
+    l.onclick = handler;
+    m.onclick = handler;
+    n.onclick = handler;
+    o.onclick = handler;
+    p.onclick = handler;
+    q.onclick = handler;
+    r.onclick = handler;
+    s.onclick = handler;
+    t.onclick = handler;
+}
+
+function handler()
+   
+function myFunction() {
+    document.getElementById("confirm").click();
+  }
