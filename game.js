175a13859c8fabb44897bdf36100b84a1697794c
sogeti-savas <savas.yigit@sogeti.com>
2021-12-21 17:08:51 +0100

    update game.js

--- game.js
+++ game.js
@@ -1,108 +1,84 @@
-window.onload = function () {
+let answer = [];
 
-    var f1a = document.getElementById("f1a");
-    f1a.onclick = handler;
-    var f1b = document.getElementById("f1b");
-    f1b.onclick = handler;
-    var f1c = document.getElementById("f1c");
-    f1c.onclick = handler;
-    var f1d = document.getElementById("f1d");
-    f1d.onclick = handler;
-    var f2a = document.getElementById("f2a");
-    f2a.onclick = handler;
-    var f2b = document.getElementById("f2b");
-    f2b.onclick = handler;
-    var f2c = document.getElementById("f2c");
-    f2c.onclick = handler;
-    var f2d = document.getElementById("f2d");
-    f2d.onclick = handler;
-    var f3a = document.getElementById("f3a");
-    f3a.onclick = handler;
-    var f3b = document.getElementById("f3b");
-    f3b.onclick = handler;
-    var f3c = document.getElementById("f3c");
-    f3c.onclick = handler;
-    var f3d = document.getElementById("f3d");
-    f3d.onclick = handler;
-    var f4a = document.getElementById("f4a");
-    f4a.onclick = handler;
-    var f4b = document.getElementById("f4b");
-    f4b.onclick = handler;
-    var f4c = document.getElementById("f4c");
-    f4c.onclick = handler;
-    var f4d = document.getElementById("f4d");
-    f4d.onclick = handler;
-    var f5a = document.getElementById("f5a");
-    f5a.onclick = handler;
-    var f5b = document.getElementById("f5b");
-    f5b.onclick = handler;
-    var f5c = document.getElementById("f5c");
-    f5c.onclick = handler;
-    var f5d = document.getElementById("f5d");
-    f5d.onclick = handler;
-    var f6a = document.getElementById("f6a");
-    f6a.onclick = handler;
-    var f6b = document.getElementById("f6b");
-    f6b.onclick = handler;
-    var f6c = document.getElementById("f6c");
-    f6c.onclick = handler;
-    var f6d = document.getElementById("f6d");
-    f6d.onclick = handler;
-    var f7a = document.getElementById("f7a");
-    f7a.onclick = handler;
-    var f7b = document.getElementById("f7b");
-    f7b.onclick = handler;
-    var f7c = document.getElementById("f7c");
-    f7c.onclick = handler;
-    var f7d = document.getElementById("f7d");
-    f7d.onclick = handler;
-    var f8a = document.getElementById("f8a");
-    f8a.onclick = handler;
-    var f8b = document.getElementById("f8b");
-    f8b.onclick = handler;
-    var f8c = document.getElementById("f8c");
-    f8c.onclick = handler;
-    var f8d = document.getElementById("f8d");
-    f8d.onclick = handler;
-    var f9a = document.getElementById("f9a");
-    f9a.onclick = handler;
-    var f9b = document.getElementById("f9b");
-    f9b.onclick = handler;
-    var f9c = document.getElementById("f9c");
-    f9c.onclick = handler;
-    var f9d = document.getElementById("f9d");
-    f9d.onclick = handler;
-    var f10a = document.getElementById("f10a");
-    f10a.onclick = handler;
-    var f10b = document.getElementById("f10b");
-    f10b.onclick = handler;
-    var f10c = document.getElementById("f10c");
-    f10c.onclick = handler;
-    var f10d = document.getElementById("f10d");
-    f10d.onclick = handler;
-    var f11a = document.getElementById("f11a");
-    f11a.onclick = handler;
-    var f11b = document.getElementById("f11b");
-    f11b.onclick = handler;
-    var f11c = document.getElementById("f11c");
-    f11c.onclick = handler;
-    var f11d = document.getElementById("f11d");
-    f11d.onclick = handler;
-    var f12a = document.getElementById("f12a");
-    f12a.onclick = handler;
-    var f12b = document.getElementById("f12b");
-    f12b.onclick = handler;
-    var f12c = document.getElementById("f12c");
-    f12c.onclick = handler;
-    var f12d = document.getElementById("f12d");
-    f12d.onclick = handler;
+fetch("https://draffelberg.github.io/QuizMaster/questions.json")
+    .then(response => response.json())
+    .then(questions => {
 
-}
+        for (a = 1; a <= 12; a++)
+            document.getElementById("f" + a).innerHTML =    a + ". " + questions[a - 1].question;                             //Laden der Frage in HTML
 
-function handler() {
 
-}
+        for (c = 1; c <= 12; c++) {                                                                             //Laden der Antwortmöglichkeiten in HTML
+            for (j = 1; j <= 4; j++) {
+                document.getElementById("f" + c + "_" + j).innerHTML = questions[c-1].answers[j-1];
+       
+            }
+            
+        }
+        
 
-function myFunction() {
-    document.getElementById("confirm").click();
+        for (i = 0; i < 12; i++) {                                                                              //Speichern der richtigen Antworten im Array
+            answer.push(questions[i].correct);
+        }
+        console.log(answer);
+    })
+
+
+    let input = document.querySelector("input[name='name']");                                               //func() disabled/enabled "Fertig"-Button
+    let button = document.querySelector("input[id='submitbtn']");
+    
+    button.disabled = true; 
+    
+    input.addEventListener("change", stateHandle);
+    
+    function stateHandle() {
+        if (document.querySelector("input[name='name']").value === "") {
+            button.disabled = true; 
+        } else {
+            button.disabled = false; 
+        }
+    }
+
+
+function submitAnswers() {                                                              //func() Button "Fertig"
+
+    var numQuestions = 12;
+    var score = 0;
+
+    var name = document.querySelector('input[name="name"]').value;
+
+    var rf1 = document.querySelector('input[name="rf1"]:checked');                  //Checkt ob Radio-Buttons geklickt worden sind
+    var rf2 = document.querySelector('input[name="rf2"]:checked');      
+    var rf3 = document.querySelector('input[name="rf3"]:checked');
+    var rf4 = document.querySelector('input[name="rf4"]:checked');
+    var rf5 = document.querySelector('input[name="rf5"]:checked');
+    var rf6 = document.querySelector('input[name="rf6"]:checked');
+    var rf7 = document.querySelector('input[name="rf7"]:checked');
+    var rf8 = document.querySelector('input[name="rf8"]:checked');
+    var rf9 = document.querySelector('input[name="rf9"]:checked');
+    var rf10 = document.querySelector('input[name="rf10"]:checked');
+    var rf11 = document.querySelector('input[name="rf11"]:checked');
+    var rf12 = document.querySelector('input[name="rf12"]:checked');
+    
+    for (i = 1; i <= numQuestions; i++) {                                       //Alert-Ausgabe, falls eine Frage übersprungen worden ist
+
+        if (eval("rf" + i) == null) {
+            alert("Bitte beantworte Frage " + i + " noch");
+            return false;
+        }
+    }
+
+    
+    
+    for (i = 1; i <= numQuestions; i++) {                                       //Zählen der richtigen Antworten
+        if (eval('rf' + i).value == answer[i - 1]) {
+            score++;
+        }
+    }
+
+                                                                                    //Ausgabe Punktzahl + Name
+
+    var results = document.getElementById('resultat');                      
+    results.innerHTML = '<h3>' + name + ', du hast <span>' + score + '</span> von <span>' + numQuestions + '</span> Punkten erzielt</h3>';
+    return false;
 }
