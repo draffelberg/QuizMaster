571fed5e08dcbe0af19f9fbef2793077003e80a1
draffelberg <96247335+draffelberg@users.noreply.github.com>
2021-12-24 13:38:43 +0100

    Update test_allFalse.py

--- test_sel/test_allFalse.py
+++ test_sel/test_allFalse.py
@@ -5,8 +5,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import *
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 # Logger Instanz: 
 
@@ -24,7 +22,6 @@ for i in range(0,12):
 logger.info("Initialisiere Webdriver.. ")
 drv = webdriver.Chrome()
 drv.get(url)
-wait = WebDriverWait(drv, 10)
 
 def test_enterName():
     logger.info("Suche Namensfeld und sende Namen")
