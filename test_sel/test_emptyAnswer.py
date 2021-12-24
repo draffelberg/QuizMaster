e4479eb6f32dd2963d5008c5d2eb9594524628bf
draffelberg <96247335+draffelberg@users.noreply.github.com>
2021-12-24 10:19:16 +0100

    Testfall emptyAnswer
    1. enters name
    2. answers every question, except for the first
    3. presses "Fertig"
    4. alert opens
    
    TEST PASSES

---
+++ test_sel/test_emptyAnswer.py
@@ -0,0 +1,91 @@
+# Imports:
+import logging
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import *
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Logger Instanz: 
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Variablen
+
+url = "https://draffelberg.github.io/QuizMaster/"
+anzahlFragen = 0 # to be overwritten
+listOfIDs = []
+
+# Driver Instanz:
+
+logger.info("Initialisiere Webdriver.. ")
+drv = webdriver.Chrome()
+drv.get(url)
+wait = WebDriverWait(drv, 10)
+
+# Wrapper Funktion
+
+def fragenZaehlen():
+    
+    logger.info("Zähle Fragen..")
+
+    try:
+        counted = len(drv.find_elements_by_tag_name("h3"))
+        global anzahlFragen
+        anzahlFragen = counted
+        logger.info("{} Fragen gefunden".format(str(counted)))
+
+    except NoSuchElementException:
+        logger.warning("Keine Fragen gefunden")
+
+def getAnswerID():
+    if anzahlFragen > 1:
+        try:
+            logger.info("Sammle IDs")
+            for i in range(0, anzahlFragen):
+                id = "f{}".format(i+1)
+                listOfIDs.append(str(id))
+        except:
+            logger.warning("Fehler bei ID Erstellung")
+
+# Testfunktionen
+
+def test_enterName():
+    logger.info("Suche Namensfeld und sende Namen")
+    try:
+        nameForm = drv.find_element_by_name("name")
+        nameForm.click()
+        nameForm.send_keys("Tester")
+    except:
+        logger.warning("Fehler bei der Namenseingabe")
+
+def test_clickAnswers():
+
+    fragenZaehlen()
+    getAnswerID()
+    logger.info("Klicke Antworten durch.. ")
+    for i in listOfIDs:
+        if i == "f1":
+            continue
+        try:
+            questionID = i
+            antworten = drv.find_elements_by_name("r{}".format(questionID))
+
+            for element in antworten:
+                element.click()
+                time.sleep(0.1)
+        except:
+            logger.warning("Antwort {} konnten nicht geklickt werden".format(str(i)))
+
+def test_submit():
+    logger.info("Suche Fertig Button..")
+    submitbutton = drv.find_element_by_id("submitbtn")
+    try:
+        submitbutton.click()
+        logger.info("Fertig Button geklickt")
+    except:
+        logger.warning("Fertig Button nicht klickbar")
+
