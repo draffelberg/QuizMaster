096f56d66976dab0684e6115852068f8f1a7e51c
draffelberg <96247335+draffelberg@users.noreply.github.com>
2021-12-24 12:28:15 +0100

    Add files via upload
    PASSES

---
+++ test_sel/test_halfCorrect.py
@@ -0,0 +1,73 @@
+# Imports:
+
+import time
+import logging
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import *
+
+# Logger Instanz:
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Variablen
+
+url = "https://draffelberg.github.io/QuizMaster/"
+listOfIDs = []
+for i in range(0,12):
+    listOfIDs.append("f{}".format(i+1))
+
+# Driver Instanz:
+
+logger.info("Intialisiere Webdirver")
+drv = webdriver.Chrome()
+drv.get(url)
+
+# Testfunktion
+
+def test_enterName():
+    logger.info("Suche Namensfeld und sende Namen")
+    try:
+        nameForm = drv.find_element_by_name("name")
+        nameForm.click()
+        nameForm.send_keys("Tester")
+        logger.info("Name erfolgreich eingegeben")
+
+    except NoSuchElementException:
+        logger.warning("Kein Namensfeld gefunden!")
+
+def test_answers():
+    
+    for i in listOfIDs:
+        logger.info("Beantworte Fragen")
+        if i == "f1":
+            try:
+                drv.find_element_by_xpath("//*[@id='container']/section/form[2]/input[4]").click()
+            except NoSuchElementException:
+                logger.warning("Frage 1 konnte nicht beantwortet werden")
+
+        elif i == "f2":
+            try:
+                drv.find_element_by_xpath("//*[@id='container']/section/form[2]/input[6]").click()
+            except NoSuchElementException:
+                logger.warning("Frage 2 konnte nicht beantwortet werden")
+        #funktioniert, weil bei 4 Fragen 1 die richtige Antwort ist.
+        else:
+            try:
+                questionID = i
+                antwort = drv.find_element_by_name("r{}".format(questionID))
+                antwort.click()
+            except:
+                logger.warning("Antwort {} konnte nicht beantwortet werden".format(questionID))
+
+def test_submit():
+    logger.info("Suche Fertig Button..")
+    submitbutton = drv.find_element_by_id("submitbtn")
+    try:
+        submitbutton.click()
+        logger.info("Fertig Button geklickt")
+    except:
+        logger.warning("Fertig Button nicht klickbar")
+
