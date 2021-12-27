485aa8f73f88917a9f5f53ddaeb330267d68a441
draffelberg <96247335+draffelberg@users.noreply.github.com>
2021-12-27 12:05:06 +0100

    Update test_quizmaster.py

--- test_sel/test_quizmaster.py
+++ test_sel/test_quizmaster.py
@@ -20,7 +20,7 @@ listOfIDs = []
 # Driver Instanz:
 
 logger.info("Initialisiere Webdriver.. ")
-drv = webdriver.Chrome()
+drv = webdriver.Chrome('/usr/local/lib/python3.7/dist-packages/selenium/webdriver/chrome/chromedriver')
 drv.get(url)
 
 # Wrapper Funktion
