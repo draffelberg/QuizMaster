89b75dddc8be74c91404d8d28241eb8e77041ca5
draffelberg <96247335+draffelberg@users.noreply.github.com>
2021-12-27 13:37:50 +0100

    Update test_fertigbuttonohnenamen.py

--- test_sel/test_fertigbuttonohnenamen.py
+++ test_sel/test_fertigbuttonohnenamen.py
@@ -5,6 +5,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import *
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 # Logger Instanz: 
 
@@ -18,9 +21,15 @@ anzahlFragen = 0 # to be overwritten
 listOfIDs = []
 
 # Driver Instanz:
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+s = Service('/var/lib/jenkins/workspace/chromedriver')
 
 logger.info("Initialisiere Webdriver.. ")
-drv = webdriver.Chrome()
+drv = webdriver.Chrome(service=s, options=chrome_options)
 drv.get(url)
 
 # Wrapper Funktion
