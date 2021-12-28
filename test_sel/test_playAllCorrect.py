09f9d0cd782702ca55ec0c1cdb31b594e6d35a07
draffelberg <96247335+draffelberg@users.noreply.github.com>
2021-12-28 14:52:12 +0100

    Update test_playAllCorrect.py
    Angepasst an blueprint

--- test_sel/test_playAllCorrect.py
+++ test_sel/test_playAllCorrect.py
@@ -1,13 +1,37 @@
-#import library
+# Imports
+import logging
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from datetime import datetime
-import time
-import logging
+from selenium.common.exceptions import *
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
+
+# Logger Instanz
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-driver = webdriver.Chrome()
+
+
+# Variablen
+url = "https://draffelberg.github.io/QuizMaster/"
+anzahlFragen = 0 # to be overwritten
+listOfIDs = []
+
+
+# Driver Insatanz
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+s = Service('/var/lib/jenkins/workspace/chromedriver')
+
+logger.info("Initialisiere Webdriver.. ")
+
+drv = webdriver.Chrome(service=s, options=chrome_options)
+drv.get(url)
 
 # variables
 url = "https://draffelberg.github.io/QuizMaster/"
