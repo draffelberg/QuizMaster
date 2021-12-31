6e90940f003c32bff00d4e3d82bcd1e902b07774
draffelberg <96247335+draffelberg@users.noreply.github.com>
2021-12-31 11:52:15 +0100

    Create script_win_blueprint.py
    Blueprint zur Testauführung unter Windows

---
+++ test_sel/script_win_blueprint.py
@@ -0,0 +1,21 @@
+# Imports
+import logging
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import *
+
+# Logger Instanz
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Variablen
+url = "https://draffelberg.github.io/QuizMaster/"
+anzahlFragen = 0 # to be overwritten
+listOfIDs = []
+
+# Driver Instanz
+logger.info("Initialisiere Webdriver.. ")
+
+drv = webdriver.Chrome()
+drv.get(url)
